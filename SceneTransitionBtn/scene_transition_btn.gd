57b37f80544d0048867e9f9e17f6422d4fc1cd4b
Sebastian Rinaldi <epicsauce2013@gmail.com>
2026-08-30 02:51:22 -0700

    Completed with Music

--- SceneTransitionBtn/scene_transition_btn.gd
+++ SceneTransitionBtn/scene_transition_btn.gd
@@ -3,11 +3,16 @@ extends Button
 
 @export_file("*.tscn") var scene_path
 
+@export var success_btn = false
+
 func _ready() -> void:
 	pass
 	#hide()
 
 
 func _on_pressed() -> void:
-	GlobalAudioManager.play_sfx(0)
+	if success_btn:
+		GlobalAudioManager.play_sfx(3)
+	else:
+		GlobalAudioManager.play_sfx(0)
 	GlobalTransition.change_scene_to(scene_path)
