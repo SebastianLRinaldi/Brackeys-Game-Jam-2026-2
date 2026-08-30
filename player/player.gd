403194a3cc1639cab987a854a67f6a4beb685c7d
Sebastian Rinaldi <epicsauce2013@gmail.com>
2026-08-29 23:17:56 -0700

    Bob assets and animations

--- player/player.gd
+++ player/player.gd
@@ -6,8 +6,9 @@ extends CharacterBody2D
 @onready var end_char_label: Label = %EndCharLabel
 @onready var dialog_container: MarginContainer = $DialogContainer
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
+const SPEED = 280.0
 const JUMP_VELOCITY = -400.0
 
 func _ready() -> void:
@@ -20,28 +21,44 @@ func _ready() -> void:
 	GlobalDialogManager.dialog_container = self.dialog_container
 	dialog_container.hide()
 
+var animation_direction: String = "down"
+var animation_state: String = ""
+
+var acceleration = 10.0
+
+func update_sprite_direction(input: Vector2) -> void:
+	match input:
+		Vector2.DOWN:
+			animation_direction = "down"
+		Vector2.UP:
+			animation_direction = "up"
+		Vector2.RIGHT:
+			animation_direction = "right"
+		Vector2.LEFT:
+			animation_direction = "left"
+
+func update_sprite() -> void:
+	if velocity.length() > 0:
+		animation_state = "walk_"
+	else:
+		animation_state ="idle_"
 
 func _physics_process(delta: float) -> void:
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
+	var direction := Input.get_vector("left", "right", "up", "down")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	var direction_v := Input.get_axis("ui_up", "ui_down")
-	if direction_v:
-		velocity.y = direction_v * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	update_sprite_direction(direction)
+	update_sprite()
+	var animation_to_run = animation_state+animation_direction
+	print(animation_to_run)
+	animated_sprite_2d.play(animation_to_run)
+
+	velocity = direction * SPEED
 
 	move_and_slide()
+
+
+
+	#velocity.x = move_toward(velocity.x, direction.x*SPEED, acceleration)
+	#velocity.y = move_toward(velocity.y, direction.y*SPEED, acceleration)
+#
+	#move_and_collide(velocity*delta)
